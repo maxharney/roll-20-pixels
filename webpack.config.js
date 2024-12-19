3381f6beaa9639f8f323a475c3f38ae6deccc22d
Max Harney <max@maxs-mbp.lan>
2024-12-19 14:55:55 -0600

    Adding TypeScript support

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,24 @@ const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+    devtool: 'inline-source-map',
     entry: {
-        popup: path.resolve(__dirname, "dist", "popup.js")
+        popup: path.resolve(__dirname, "src", "popup.ts")
     },
     output: {
         clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
     },
     plugins: [
         new CopyPlugin({
